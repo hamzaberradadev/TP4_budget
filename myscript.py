4b15f0f159b6f728d86e0640104bc213d514a29c
Hamza Berrada <hamza.berrada@bell.ca>
2023-03-24 22:15:29 -0400

    question C

--- myscript.py
+++ myscript.py
@@ -1,28 +1,46 @@
-import os
-import json
-## we will create a cash file to store the last good commit after each good push to the server
-cash_file = 'last_good_commit.json'
-good_commit = None
-## we will open and read the cash file
-with open(cash_file, 'r') as f:
-    good_commit = json.load(f)['hash']
+# import os
+# import json
+# ## we will create a cash file to store the last good commit after each good push to the server
+# cash_file = 'last_good_commit.json'
+# good_commit = None
+# ## we will open and read the cash file
+# with open(cash_file, 'r') as f:
+#     good_commit = json.load(f)['hash']
 
-## we will get the current commit hash
-last_commit = os.popen('git rev-parse HEAD').read().strip()
+# ## we will get the current commit hash
+# last_commit = os.popen('git rev-parse HEAD').read().strip()
 
-## we will run the git bisect command
+# ## we will run the git bisect command
 
-print (last_commit+' '+good_commit)
-os.system('git bisect start %s %s' % (last_commit, good_commit))
-the_output_of_bisect = os.popen('git bisect run python manage.py test').read()
-os.popen('git bisect bad').read()
-os.popen('git bisect reset').read()
+# print (last_commit+' '+good_commit)
+# os.system('git bisect start %s %s' % (last_commit, good_commit))
+# the_output_of_bisect = os.popen('git bisect run python manage.py test').read()
+# os.popen('git bisect bad').read()
+# os.popen('git bisect reset').read()
 
-## we will check if the output of the bisect command is the bad commit
-print('################################# \n %s' % the_output_of_bisect)
-if the_output_of_bisect.find('is the first bad commit') != -1:
-    ## then raise an error to stop the bisect
-    raise Exception('Bisect found the bad commit')
+# ## we will check if the output of the bisect command is the bad commit
+# print('################################# \n %s' % the_output_of_bisect)
+# if the_output_of_bisect.find('is the first bad commit') != -1:
+#     ## then raise an error to stop the bisect
+#     raise Exception('Bisect found the bad commit')
 
-with open(cash_file, 'w') as f:
-    json.dump({'hash': last_commit}, f, indent=4)
+# with open(cash_file, 'w') as f:
+#     json.dump({'hash': last_commit}, f, indent=4)
+
+import os 
+import sys
+import subprocess
+
+result = subprocess.run(['python', 'manage.py', 'test'], capture_output=True, text=True)
+
+if result.returncode == 0:
+    print("Tous les tests ont réussi !")
+else:
+    print("Des erreurs ont été trouvées lors de l'exécution des tests :\n")
+    print(result.stdout)
+    print(result.stderr)
+    bad_commit = sys.argv [1]
+    good_commit = sys. argv [2]
+    os. system('git bisect start *s %s' % (bad_commit, good_commit)) 
+    os.system('git bisect run python manage.py test') 
+    os.system('git bisect reset')
